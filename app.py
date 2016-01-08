a7e74c239c1013b398658719aac0dcab7db3092f
Brice Johnson <bricej13@gmail.com>
2016-01-07 22:15:39 -0600

    Added top processes and $ for memory

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
 from flask import Flask, Response
 from datetime import datetime, timedelta, time
-import os
-import subprocess
-import json
-import re
+import os, subprocess, json, re
+
+# Configuration
+disks = ['/media/1tb', '/media/terry', '/media/320gb']
+
+# End Configuration
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def status():
     serverstatus = {}
+
+    # Comment out any of these lines to remove them from resulting JSON
     serverstatus['disks'] = get_disk_space()
     serverstatus['memory'] = get_memory()
     serverstatus['cpu'] = get_cpu()
     serverstatus['power'] = get_power()
+    serverstatus['processes'] = get_processes()
+
     lastmodified = datetime.now() - timedelta(minutes=1)
     headers = {'Last-Modified': lastmodified.ctime()}
     headers = {'Etag': datetime.now().ctime()}
     return Response(response=json.dumps(serverstatus), status=200, mimetype='application/json', headers=headers)
-    #return str(", ".join(disks[1]))
 
 def get_disk_space():
-    disks = ['/media/1tb', '/media/terry', '/media/320gb']
     result = {}
     for disk in disks:
         df = subprocess.Popen(["df", "-h", disk], stdout=subprocess.PIPE)
         output = df.communicate()[0]
-        device, size, used, available, percent, mountpoint = \
-            output.split("\n")[1].split()
-        # result.append(output.split("\n")[1])
-        result[disk] = {
-            'name': disk,
+        device, size, used, available, percent, mountpoint = output.split("\n")[1].split()
+        result[mountpoint] = {
             'device': device,
             'size': size,
             'used': used,
@@ -52,7 +54,10 @@ def get_memory():
             'free': byte_to_human(free),
             'shared': byte_to_human(shared),
             'buffers': byte_to_human(buffers),
-            'cached': byte_to_human(cached)}
+            'cached': byte_to_human(cached),
+            'percent': "{:.0%}".format((float(used) - float(cached))/float(total)),
+            }
+            
     type, total, used, free = \
         output.split("\n")[3].split()
     memory['swap'] = {
@@ -60,9 +65,9 @@ def get_memory():
             'total': byte_to_human(total),
             'used': byte_to_human(used),
             'free': byte_to_human(free),
+            'percent': "{:.0%}".format((float(used))/float(total)),
             }
     return memory
-
 
 def get_cpu():
     uptime = subprocess.Popen(["uptime"], stdout=subprocess.PIPE)
@@ -80,7 +85,6 @@ def get_cpu():
         "uptime": " ".join(servertime.split()[-2:]),
         "temperature": get_cpu_temp()
         }
-    #return output.split("\n")[0].split()[-3:]
 
 def get_cpu_temp():
     uptime = subprocess.Popen(["cat", "/proc/acpi/thermal_zone/TZ01/temperature"], stdout=subprocess.PIPE)
@@ -92,16 +96,6 @@ def get_cpu_temp():
     max_temp = output.strip().split("\n")[0].split()[2]
     return {"current_temp": cur_temp + "C", "critical_temp": max_temp + "C"}
 
-def byte_to_human(kb):
-    mb = float(kb) / 1024
-    gb = mb / 1024
-    if gb > 1:
-        return format(gb, '.1f') + "g"
-    elif mb > 1:
-        return format(mb, '.1f') + "m"
-    else:
-        return str(kb) + "k"
-
 def get_power():
     pwrstatus = subprocess.Popen(["pwrstat", "-status"], stdout=subprocess.PIPE)
     output = pwrstatus.communicate()[0]
@@ -112,6 +106,27 @@ def get_power():
             parts = l.split("\t")
             power[parts[0]] = parts[1].strip()
     return power
+
+def get_processes():
+    cmd = subprocess.Popen(["ps", "-eo", "pcpu,comm"], stdout=subprocess.PIPE)
+    output = cmd.communicate()[0]
+    procs = []
+    proclist = sorted(output.split("\n"), reverse=True)
+    for proc in proclist[1:11]:
+        procdata = proc.strip().split(" ", 1)
+        procs.append({"process": procdata[1], "cpu": procdata[0]})
+    
+    return procs
+
+def byte_to_human(kb):
+    mb = float(kb) / 1024
+    gb = mb / 1024
+    if gb > 1:
+        return format(gb, '.1f') + "g"
+    elif mb > 1:
+        return format(mb, '.1f') + "m"
+    else:
+        return str(kb) + "k"
 
 if __name__ == '__main__':
     app.debug = True
